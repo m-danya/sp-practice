5bb4763f3da0d21280a8a48a67873dc63604405b
m-danya <danila-mikh@ya.ru>
2021-03-03 23:37:06 +0300

    configure argparse, initialise joint dataframe

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # sp-practice
 
 This script calculates some statistics about the interaction traces from the [Rico dataset](http://interactionmining.org/rico)
+
+### Usage
+```
+get_statistics.py [-h] -i I
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -i I        set input traces directory path (D must directly contain folders with package names)
+```
--- get_statistics.py
+++ get_statistics.py
@@ -1,1 +1,30 @@
-print('Hello, world!')
+#! /usr/bin/python3
+
+import argparse
+import glob
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", help=\
+    "set input traces directory path (D must directly contain folders with package names)", required=True)
+args = parser.parse_args()
+path = args.i
+
+if path[-1] != '/':
+    path += '/'
+
+df = pd.DataFrame()
+dfs = []
+
+for application in glob.glob(path + "*")[:30]:
+    for trace in glob.glob(application + "/trace*"):
+        gestures_file = "trace/gestures.json"
+        for view in glob.glob(trace + "/view_hierarchies/*.json"):
+            df_temp = pd.read_json(view)
+            dfs.append(df_temp)
+
+df = pd.concat(dfs, ignore_index=True)
+
+# TODO: explore 'activity' column -- GUI elements tree to obtain statistics
+
+print(df)
