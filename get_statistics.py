130ecdb48b71b39ebd4863332b4fb2f7c98edf06
m-danya <danila-mikh@ya.ru>
2021-03-06 22:40:53 +0300

    change data adding stategy: parsing jsons without adding to df immediately. this gave an increase of 60% in runtime. add some new statistics: taps/swaps, top-10 elements, clickable elements count. add cache-cleaning script for fair performance testing. add requirements.txt

--- get_statistics.py
+++ get_statistics.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i", help=\
     "set input traces directory path (D must directly contain folders with package names)", required=True)
 parser.add_argument("-f", help=\
-    "set number of first traces to process (default = inf)", required=False)
+    "set number of first traces to process (default: process all of them)", required=False)
 
 args = parser.parse_args()
 path = args.i
@@ -18,43 +18,81 @@ if args.f:
     args.f = int(args.f)
 if path[-1] != '/':
     path += '/'
-df = pd.DataFrame()
-dfs = []
 
-def dfs_search(tree):
+element_names = {}
+
+def gui_tree_dfs(tree):
     ans = {
-        'count': 1
-        # TODO: taps / swipes count, ...
+        'count': 0,
+        'clickable_elements': 0,
     }
     if tree is None:
-        return {'count': 0}
+        return ans
+    ans['count'] = 1
+    ans['clickable_elements'] = int(tree['clickable'])
+    if tree['class'] in element_names:
+        element_names[tree['class']] += 1
+    else:
+        element_names[tree['class']] = 1
+
     if 'children' not in tree:
         return ans
     for child in tree['children']:
-        child_ans = dfs_search(child)
+        child_ans = gui_tree_dfs(child)
         ans['count'] += child_ans['count']
+        ans['clickable_elements'] += child_ans['clickable_elements']
     return ans
+
+gui_rows = []
+traces_rows = []
 
 for application in tqdm(glob.glob(path + "*")[:args.f]):
     for trace in glob.glob(application + "/trace*"):
-        gestures_file = "trace/gestures.json"
+        with open(trace + "/gestures.json") as json_file:
+            gestures = json.load(json_file)
+        taps = 0
+        swipes = 0
+        for screen in gestures:
+            if len(gestures[screen]) > 1:
+                swipes += 1
+            else:
+                taps += 1
+        traces_rows.append({'taps': taps, 'swipes': swipes})
         for view in glob.glob(trace + "/view_hierarchies/*.json"):
-            # TODO: do not convert json to df, calculate values from dict and add them to THE ONLY main df as a row!
-            df_temp = pd.read_json(view)
-            if df_temp.empty:
+            with open(view) as json_file:
+                current_json = json.load(json_file)
+            if not current_json:
                 continue
-            df_temp = df_temp.drop(['active_fragments', 'added_fragments'])
-            df_temp = df_temp.drop(columns=['is_keyboard_deployed', 'request_id'])
-            stats = dfs_search(df_temp['activity']['root'])
-            for key in stats:
-                df_temp[key] = [stats[key]]
-            df_temp
-            dfs.append(df_temp)
+            stats = gui_tree_dfs(current_json['activity']['root'])
+            stats['activity_name'] = current_json['activity_name']
+            gui_rows.append(stats)
 
-df = pd.concat(dfs, ignore_index=True)
+if not traces_rows:
+    print('No traces found!')
+    exit()
+if not gui_rows:
+    print('No gui trees found!')
+    exit()
 
-print(df)
+df_gui = pd.DataFrame(gui_rows)
+df_traces = pd.DataFrame(traces_rows)
+
 print('\n')
 print('#' * 30, end='\n\n')
-print('Mean number of elements in the GUI screen tree = ', df['count'].mean())
+print('GUI STATISTICS')
 print()
+print('Mean number of elements in the GUI screen tree = %.3f' % df_gui['count'].mean())
+print('Mean number of clickable elements in the GUI screen tree = %.3f' % df_gui['clickable_elements'].mean())
+print()
+print('Top-10 elements:')
+print()
+for k, v in sorted(element_names.items(), key=lambda item: -item[1])[:10]:
+    print(f'{k}: {v} times')
+print()
+print('#' * 30, end='\n\n')
+print('TRACES STATISTICS')
+print()
+print('Mean number of taps per trace = %.3f' % df_traces['taps'].mean())
+print('Mean number of swipes per trace = %.3f' % df_traces['swipes'].mean())
+print()
+
