22a9f1e6523d3e0c527df9a47b8e8b505a79316d
m-danya <danila-mikh@ya.ru>
2021-03-11 21:57:49 +0300

    Split the code into few functions
    Some parts of the main function was placed in separate functions
    to increase the readability of code and prepare for adding tests

--- get_statistics.py
+++ get_statistics.py
@@ -5,8 +5,27 @@ from pathlib import Path
 import pandas as pd
 from tqdm import tqdm
 import json
-import glob
 from collections import defaultdict
+
+
+def get_gui_stats(tree_json, element_names):
+    stats = get_subtree_stats(tree_json['activity']['root'], element_names)
+    stats['activity_name'] = tree_json['activity_name']
+    return stats
+
+
+def get_traces_stats(gestures_json):
+    taps, swipes = 0, 0
+    for screen in gestures_json:
+        if len(gestures_json[screen]) > 1:
+            swipes += 1
+        else:
+            taps += 1
+    return {
+        'taps': taps,
+        'swipes': swipes,
+        'length': len(gestures_json)
+    }
 
 
 def get_subtree_stats(tree, element_names):  # dfs
@@ -20,71 +39,14 @@ def get_subtree_stats(tree, element_names):  # dfs
     collected_data['clickable_elements'] = int(tree['clickable'])
     element_names[tree['class']] += 1
 
-    for child in tree.get('children', []):  # default children = [] instead of None
+    for child in tree.get('children', []):  # default = [] instead of None
         child_ans = get_subtree_stats(child, element_names)
         collected_data['count'] += child_ans['count']
         collected_data['clickable_elements'] += child_ans['clickable_elements']
     return collected_data
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i",
-                        help="set input traces directory path (D must directly contain folders with package names)",
-                        required=True,
-                        type=Path)
-    parser.add_argument("-n",
-                        help="set number of first traces to process (default: process all of them)",
-                        required=False,
-                        type=int)
-
-    args = parser.parse_args()
-    path = args.i
-
-    element_names = defaultdict(int)
-
-    gui_rows = []
-    traces_rows = []
-
-    for application in tqdm([x for x in path.iterdir() if x.is_dir()][:args.n]):
-        # args.n == None is working fine
-        for trace in [x for x in application.iterdir() if x.is_dir()]:
-            with open(trace / "gestures.json") as json_file:
-                gestures = json.load(json_file)
-            taps, swipes = 0, 0
-            for screen in gestures:
-                if len(gestures[screen]) > 1:
-                    swipes += 1
-                else:
-                    taps += 1
-            traces_rows.append({
-                'taps': taps,
-                'swipes': swipes,
-                'length': len(gestures)
-            })
-            for view in (trace / "view_hierarchies").glob("*.json"):
-                if view.name.startswith('.'):
-                    # ignore hidden files
-                    continue
-                with open(view) as json_file:
-                    current_json = json.load(json_file)
-                if not current_json:
-                    continue
-                stats = get_subtree_stats(
-                    current_json['activity']['root'], element_names)
-                stats['activity_name'] = current_json['activity_name']
-                gui_rows.append(stats)
-
-    if not traces_rows:
-        print('No traces found!')
-        exit()
-    if not gui_rows:
-        print('No gui trees found!')
-        exit()
-
-    df_gui = pd.DataFrame(gui_rows)
-    df_traces = pd.DataFrame(traces_rows)
-
+def print_statistics(df_gui, df_traces, element_names):
     print('\n')
     print('#' * 30, end='\n\n')
     print('GUI STATISTICS')
@@ -105,10 +67,61 @@ def main():
     print('Mean number of taps per trace = %.3f' % df_traces['taps'].mean())
     print('Mean number of swipes per trace = %.3f' %
           df_traces['swipes'].mean())
-
     print('Mean trace length = %.3f' %
           df_traces['length'].mean())
     print()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i",
+                        help="set input traces directory path (D must directly \
+                            contain folders with package names)",
+                        required=True,
+                        type=Path)
+    parser.add_argument("-n",
+                        help="set number of first traces to process (default: \
+                            process all of them)",
+                        required=False,
+                        type=int)
+
+    args = parser.parse_args()
+    path = args.i
+
+    element_names = defaultdict(int)
+
+    gui_rows, traces_rows = [], []
+
+    application_folders = [x for x in path.iterdir() if x.is_dir()][:args.n]
+    # args.n == None is working fine
+
+    for application in tqdm(application_folders):
+        for trace in [x for x in application.iterdir() if x.is_dir()]:
+            with open(trace / "gestures.json") as json_file:
+                gestures = json.load(json_file)
+            traces_rows.append(get_traces_stats(gestures))
+            for view in (trace / "view_hierarchies").glob("*.json"):
+                if view.name.startswith('.'):
+                    # ignore hidden files
+                    continue
+                with open(view) as json_file:
+                    current_json = json.load(json_file)
+                if not current_json:
+                    # skip empty jsons
+                    continue
+                gui_rows.append(get_gui_stats(current_json, element_names))
+
+    if not traces_rows:
+        print('No traces found!')
+        exit()
+    if not gui_rows:
+        print('No gui trees found!')
+        exit()
+
+    df_gui = pd.DataFrame(gui_rows)
+    df_traces = pd.DataFrame(traces_rows)
+
+    print_statistics(df_gui, df_traces, element_names)
 
 
 if __name__ == '__main__':
